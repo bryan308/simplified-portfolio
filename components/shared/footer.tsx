fc34d7368fe1eb6469776d2197dd0b9ba2b19541
bryan308 <cdrcangulo@gmail.com>
2024-12-16 19:54:55 +0800

    add footer link to Bryl Lim's website; enhance footer layout and accessibility

--- components/shared/footer.tsx
+++ components/shared/footer.tsx
@@ -2,6 +2,7 @@
 
 import React from "react"
 import { motion } from "motion/react"
+import Link from "next/link"
 
 const Footer: React.FC = () => {
 	return (
@@ -10,9 +11,21 @@ const Footer: React.FC = () => {
 			animate={{ opacity: 1, filter: "blur(0)" }}
 			exit={{ opacity: 1, y: "0", filter: "blur(0)" }}
 			transition={{ delay: 2, duration: 0.75, ease: "easeInOut" }}
-			className="max-w-4xl mx-auto border-t mt-20 py-6 md:py-12"
+			className="flex flex-col md:flex-row justify-between max-w-4xl mx-auto border-t mt-20 py-6 md:py-12"
+			aria-label="Footer"
 		>
 			<p className="text-sm">© 2024 Cedric Angulo. All rights reserved.</p>
+			<p className="text-sm">
+				Inspired by the work of Sir{" "}
+				<Link
+					href="https://bryllim.com"
+					target="_blank"
+					aria-label="Bryl Lim's website"
+					className="underline underline-offset-1 hover:underline-offset-2 transition-all"
+				>
+					Bryl Lim
+				</Link>
+			</p>
 		</motion.footer>
 	)
 }
